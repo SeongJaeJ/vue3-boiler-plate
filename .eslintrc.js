a75a038ba53619f3ce763a26614604704640ae7f
jay <jay@crossangle.io>
2023-07-05 18:40:43 +0900

    feat: lint 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,11 @@
+const RULE = {
+  OFF: 'off',
+  WARN: 'warn',
+  ERROR: 'error',
+  ALWAYS: 'always',
+  SINGLE: 'single'
+}
+
 module.exports = {
   root: true,
   env: {
@@ -7,30 +15,50 @@ module.exports = {
     'vue/setup-compiler-macros': true,
   },
   'extends': [
+    'plugin:vue/vue3-strongly-recommended',
     'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/typescript/recommended'
+    'plugin:vue-scoped-css/vue3-recommended',
+    '@vue/typescript/recommended',
+    'eslint:recommended'
   ],
+  plugins: [ '@typescript-eslint', 'simple-import-sort', ],
+  parser: 'vue-eslint-parser',
   parserOptions: {
-    ecmaVersion: 2020
+    parser: '@typescript-eslint/parser',
+    ecmaVersion: 'latest',
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'vue/multi-word-component-names': 'off',
-    indent: ['error', 2],
-    quotes: ['error', 'single'],
-    semi: ['error', 'never'],
+    'no-console': process.env.NODE_ENV === 'production' ? RULE.WARN : RULE.OFF,
+    'no-debugger': process.env.NODE_ENV === 'production' ? RULE.WARN : RULE.OFF,
+    'no-var': RULE.ERROR,
+    'no-unused-vars': RULE.WARN,
+    'vue/multi-word-component-names': RULE.OFF,
+    'object-curly-spacing': [ RULE.ERROR, RULE.ALWAYS ],
+    'array-bracket-spacing': [ RULE.ERROR, RULE.ALWAYS ],
+    'comma-spacing': [ RULE.ERROR, { 'before': false, 'after': true } ],
+    'keyword-spacing': [ RULE.WARN, { 'after': true } ],
+    indent: [ RULE.ERROR, 2 ],
+    quotes: [ RULE.ERROR, RULE.SINGLE ],
+    'vue/valid-v-slot': [
+      RULE.ERROR,
+      {
+        allowModifiers: false,
+      },
+    ],
+    'simple-import-sort/imports': RULE.WARN,
+    'simple-import-sort/exports': RULE.WARN,
+    semi: [ RULE.ERROR, 'never' ],
+    'linebreak-style': [ RULE.ERROR, 'unix' ],
     'no-restricted-imports': [
-      'error',
+      RULE.ERROR,
       {
         patterns: [
           {
-            group: ['src/*'],
+            group: [ 'src/*' ],
             message: '경로는 최초 @로 시작해주십시오.',
           },
           {
-            group: ['../*'],
+            group: [ '../*' ],
             message: '경로는 최초 @로 시작해주십시오.',
           },
         ],
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,9 +7,10 @@
 <script setup lang="ts">
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 .home {
-//
+  width: 100%;
+  height: 90vh;
 }
 </style>
  
